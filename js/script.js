e2c753639f94c2a69c26bcf23bce41f72c09efd1
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-11 02:07:48 +0530

    Added swipper code

--- js/script.js
+++ js/script.js
@@ -81,12 +81,8 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: true
+    }
   });
