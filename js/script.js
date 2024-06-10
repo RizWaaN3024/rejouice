648711562530b041e271aff59d43da7782b984c2
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-11 01:21:04 +0530

    Added Swiper

--- js/script.js
+++ js/script.js
@@ -76,3 +76,17 @@ function page2Animation() {
 }
 
 page2Animation();
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
