143641e94ce3f956a44429d2144e7a427d413381
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-18 16:40:33 +0530

    code fixes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 function locomotiveScroll() {
     gsap.registerPlugin(ScrollTrigger);
 
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
 const locoScroll = new LocomotiveScroll({
   el: document.querySelector("#main"),
   smooth: true
